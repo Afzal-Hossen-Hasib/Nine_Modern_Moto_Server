91006bef58ebcff00dbd8c41ee9315cb91c9d6f8
afzalhossenhasib <ahhasib47@gmail.com>
2022-05-22 23:27:12 +0600

    single id added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT||5000;
 
@@ -16,13 +16,23 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function  run() {
     try {
         await client.connect();
+
         const partsCollection = client.db('modern_moto').collection('parts');
+
 
         app.get('/part', async (req, res) => {
             const query = {};
             const cursor = partsCollection.find(query);
             const parts = await cursor.toArray();
             res.send(parts);
+
+        app.get('/part/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const part = await partsCollection.findOne(query);
+            res.send(part);
+        })
+
         })
     }
     finally {
