7a122680b74070c3b42f06fa5dc50f1a4b3cab85
afzalhossenhasib <ahhasib47@gmail.com>
2022-05-22 21:38:26 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -11,7 +11,18 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sowhm.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function  run() {
+    try {
+        await client.connect();
+        console.log('data connect');
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
