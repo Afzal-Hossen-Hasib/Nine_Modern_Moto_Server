141cc1e2eeda39195e3a0eb5672fbcb158112059
afzalhossenhasib <ahhasib47@gmail.com>
2022-05-23 21:22:50 +0600

    delete order

--- index.js
+++ index.js
@@ -55,7 +55,12 @@ async function  run() {
         res.send (productInfo);
       });
 
-      
+      app.delete('myorder/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = {_id: ObjectId (id)};
+        const result = await myOrderCollection.deleteOne(query);
+        res.send (result);
+      })
 
 
     }
